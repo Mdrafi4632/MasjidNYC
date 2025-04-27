9d605315a896de2d5236290704b13f61ed6e124a
Md Rafi <rafiusa19@gmail.com>
2025-04-27 03:11:08 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,7 @@
+// src/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // <-- notice the correct name!
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
